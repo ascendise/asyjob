19ea47d44c8ace00c1f10766e4d7042f0394190b
Ascendise <rapperido@gmail.com>
2024-02-27 08:57:28 +0100

    define job running interface

---
+++ AsyJob/Jobs/JobController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyJob.Jobs
+{
+    [Route("api/jobs")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpPost]
+        public Task<JobResponseDto> RunJob(JobRequestDto jobRequest)
+        {
+            return Task.FromResult((null as JobResponseDto)!);
+        }
+    }
+}
---
+++ AsyJob/Jobs/JobDto.cs
@@ -0,0 +1,15 @@
+﻿namespace AsyJob.Jobs
+{
+    public class JobRequestDto(string jobtype, string name = "", string description = "", object? input = null)     
+    {
+        public string Name { get; private set; } = name;
+        public string Description { get; private set; } = description;
+        public string JobType { get; private set; } = jobtype;
+        public object? Input { get; private set; } = input;
+    }
+
+    public class JobResponseDto(Job job)
+    {
+        public Job Job { get; private set; } = job;
+    }
+}
