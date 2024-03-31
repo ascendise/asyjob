a3e3eee468ab474012d8cb78c60376386eeec45d
Ascendise <rapperido@gmail.com>
2024-03-31 22:18:35 +0200

    permission based authorize attribute

---
+++ AsyJob/Auth/HasRightsAttribute.cs
@@ -0,0 +1,24 @@
+﻿using AsyJob.Lib.Auth;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AsyJob.Auth
+{
+    /// <summary>
+    /// Sets a policy in the format "HasRights_{<see cref="Right.ToString"></see>}
+    /// </summary>
+    internal class HasRightsAttribute : AuthorizeAttribute
+    {
+        const string POLICY_PREFIX = "HasRights";
+
+        public Right Right
+        {
+            set
+            {
+                Policy = ToPolicy(value);
+            }
+        }
+
+        private static string ToPolicy(Right right)
+            => $"{POLICY_PREFIX}_{right}";
+    }
+}
