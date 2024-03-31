a8853fa7cb2aa532d83c6e3926f6d4e1b2120570
Ascendise <rapperido@gmail.com>
2024-03-31 21:11:11 +0200

    add rights to IdentityUser

--- AsyJob/Auth/User.cs
+++ AsyJob/Auth/User.cs
@@ -1,18 +1,21 @@
 ﻿using AspNetCore.Identity.MongoDbCore.Models;
 using AsyJob.Lib.Auth;
+using AsyJob.Lib.Runner;
 
 namespace AsyJob.Auth
 {
     public class User : MongoIdentityUser<Guid>
     {
+        public IEnumerable<Right> Rights { get; set; } = [
+            new Right(nameof(JobRunner), Operation.Read | Operation.Write | Operation.Execute)    
+        ];
+
         public User(string username) : base(username) { }
         public User() : base() { }
 
         public Lib.Auth.User GetDomainUser()
         {
-            var claimToRightConverter = new ClaimToRightConverter();
-            var rights = Claims.Select(claimToRightConverter.ToRight);
-            return new(Id, UserName!, rights);
+            return new(Id, UserName!, Rights);
         }
     }
 }
