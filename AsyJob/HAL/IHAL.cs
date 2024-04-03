9541948614603dc5bcf6e97ab25ed1b16bac8876
Ascendise <rapperido@gmail.com>
2024-04-03 20:27:23 +0200

    base for hal dto structure

---
+++ AsyJob/HAL/IHAL.cs
@@ -0,0 +1,18 @@
+﻿namespace AsyJob.Web.HAL
+{
+    public interface IHAL
+    {
+        public ILinks Links { get; set; }
+        public IEmbedded Embedded { get; set; }
+    }
+
+    public interface ILinks
+    {
+
+    }
+
+    public interface IEmbedded
+    {
+
+    }
+}
