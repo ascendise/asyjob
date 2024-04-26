817c7d2030ebe3f16c6f7ddd0dc46c7aa6d9d5c2
Ascendise <rapperido@gmail.com>
2024-04-26 20:23:55 +0200

    wip: tests for UserController

--- AsyJob.Web.Tests/Auth/UsersControllerTests.cs
+++ AsyJob.Web.Tests/Auth/UsersControllerTests.cs
@@ -1,11 +1,9 @@
 ﻿using AsyJob.Lib.Auth;
 using AsyJob.Lib.Auth.Users;
-using AsyJob.Lib.Client.Abstract.Users;
 using AsyJob.Lib.Client.Users;
 using AsyJob.Lib.Tests.TestDoubles;
 using AsyJob.Web.Auth;
 using AsyJob.Web.Tests.TestDoubles;
-using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +14,8 @@ namespace AsyJob.Web.Tests.Auth
 {
     internal class UsersControllerTests
     {
-        private readonly Web.Auth.User _admin = new("admin")
+        private Web.Auth.User _admin = new Web.Auth.User("admin")
         {
-            ConfirmedByAdmin = true,
             Rights = [
                 new Right("Users", Operation.Read | Operation.Write)
             ]
@@ -29,241 +26,16 @@ namespace AsyJob.Web.Tests.Auth
         {
             //Arrange
             var user = new Web.Auth.User("HelloWorld");
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser(), user.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
+            var fakeUserRepo = new FakeUserRepository([_admin.GetDomainUser(), user.GetDomainUser()]);
+            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.GetDomainUser());
             var usersApi = new UsersApi(userManager);
             var fakeAspUserManager = new FakeAspUserManager([_admin, user]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
+            var sut = new UsersController(usersApi, fakeAspUserManager);
             //Act
             var users = await sut.GetUsers();
             //Assert
             Assert.That(users.Count(), Is.EqualTo(2));
         }
 
-        [Test]
-        public async Task GetUsers_Response_ShouldIncludeLinks()
-        {
-            //Arrange
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var response = await sut.GetUsers();
-            //Assert
-            var userResponse = response.Single();
-            Assert.Multiple(() =>
-            {
-                Assert.That(userResponse.Links.Any(l => l.Key == "users"), "users link missing");
-                Assert.That(userResponse.Links.Any(l => l.Key == "self"), "user link missing");
-            });
-        }
-
-        [Test]
-        public async Task GetUsers_UnconfirmedUser_ShouldIncludeConfirmLink()
-        {
-            //Arrange
-            var unconfirmedUser = new Web.Auth.User("Unconfirmed")
-            {
-                ConfirmedByAdmin = false
-            };
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser(), unconfirmedUser.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin, unconfirmedUser]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var response = await sut.GetUsers();
-            //Assert
-            var userResponse = response.Single(u => u.Username == "Unconfirmed");
-            Assert.That(userResponse.Links.Any(l => l.Key == "confirm"), "confirm link missing");
-        }
-
-        [Test]
-        //Confirming is a write action, so the readonly user does not care about it
-
-        public async Task GetUsers_ReadOnlyUserRights_ShouldNotIncludeConfirmLink()
-        {
-            //Arrange
-            var readonlyUser = new Web.Auth.User("ReadOnly")
-            {
-                ConfirmedByAdmin = true,
-                Rights = [new Right("Users", Operation.Read)]
-            };
-            var unconfirmedUser = new Web.Auth.User("Unconfirmed")
-            {
-                ConfirmedByAdmin = false
-            };
-            var fakeUserRepo = new FakeUserRepository([readonlyUser.ToDomainUser(), unconfirmedUser.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, readonlyUser.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([readonlyUser, unconfirmedUser]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(readonlyUser));
-            //Act
-            var response = await sut.GetUsers();
-            //Assert
-            var userResponse = response.Single(u => u.Username == "Unconfirmed");
-            Assert.That(!userResponse.Links.Any(l => l.Key == "confirm"),
-                "confirm link is present, but user does not have required rights");
-        }
-
-        [Test]
-        public async Task Update_UserExists_ShouldUpdateUser()
-        {
-            //Arrange
-            var user = new Web.Auth.User("User");
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser(), user.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin, user]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var updateRequest = new UserUpdateRequest(user.Id, "NewUsername", ["Test_rwx"]);
-            var response = await sut.Update(user.Id, updateRequest);
-            //Assert
-            var newUser = fakeUserRepo.Users.Single(u => u.Id == user.Id);
-            Assert.That(newUser.Username, Is.EqualTo("NewUsername"));
-            Assert.Multiple(() =>
-            {
-                Assert.That(newUser.Rights.Single().Resource, Is.EqualTo("Test"));
-                Assert.That(newUser.Rights.Single().Ops, Is.EqualTo(Operation.Read | Operation.Write | Operation.Execute));
-            });
-        }
-
-        [Test]
-        public void Update_UserDoesNotExist_ShouldThrowException()
-        {
-            //Arrange
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var userId = Guid.Parse("686131d6-2bf2-4d7e-8700-405fdd7ccd57");
-            var updateRequest = new UserUpdateRequest(userId, "NewUsername", ["Test_rwx"]);
-            async Task<HalUserResponse> update() => await sut.Update(userId, updateRequest);
-            //Assert
-            Assert.ThrowsAsync<KeyNotFoundException>(() => update());
-        }
-
-        [Test]
-        public async Task Update_UserExists_ShouldIncludeLinksInResponse()
-        {
-            //Arrange
-            var user = new Web.Auth.User("User")
-            {
-                ConfirmedByAdmin = false
-            };
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser(), user.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin, user]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var updateRequest = new UserUpdateRequest(user.Id, "NewUsername", ["Test_rwx"]);
-            var response = await sut.Update(user.Id, updateRequest);
-            //Assert
-            Assert.Multiple(() =>
-            {
-                Assert.That(response.Links.Any(l => l.Key == "self"), "self link missing");
-                Assert.That(response.Links.Any(l => l.Key == "users"), "users link missing");
-                Assert.That(response.Links.Any(l => l.Key == "confirm"), "confirm link missing");
-            });
-        }
-
-        [Test]
-        public async Task GetUnconfirmedUsers_HasUsers_ShouldReturnUsers()
-        {
-            //Arrange
-            var unconfirmed = new Web.Auth.User("unconfirmed")
-            {
-                ConfirmedByAdmin = false
-            };
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser(), unconfirmed.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin, unconfirmed]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var response = await sut.GetUnconfirmedUsers();
-            //Assert
-            Assert.That(response.Single().Username, Is.EqualTo(unconfirmed.UserName));
-        }
-
-        [Test]
-        public async Task GetUnconfirmedUsers_Links_ShouldContainCorrectLinks()
-        {
-            //Arrange
-            var unconfirmed = new Web.Auth.User("unconfirmed")
-            {
-                ConfirmedByAdmin = false
-            };
-            var fakeUserRepo = new FakeUserRepository([_admin.ToDomainUser(), unconfirmed.ToDomainUser()]);
-            var userManager = new UserManager(new AuthorizationManager(), fakeUserRepo, _admin.ToDomainUser());
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin, unconfirmed]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            var response = await sut.GetUnconfirmedUsers();
-            //Assert
-            var userResponse = response.Single();
-            Assert.Multiple(() =>
-            {
-                Assert.That(userResponse.Links.Any(l => l.Key == "self"), "self is missing");
-                Assert.That(userResponse.Links.Any(l => l.Key == "users"), "users is missing");
-                Assert.That(userResponse.Links.Any(l => l.Key == "confirm"), "confirm is missing");
-            });
-        }
-
-        [Test]
-        public async Task ConfirmUser_UserExists_ShouldActivateUser()
-        {
-            //Arrange
-            var unconfirmed = new Web.Auth.User("unconfirmed")
-            {
-                ConfirmedByAdmin = false
-            };
-            var userManager = new UserManager(null!, null!, null!);
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin, unconfirmed]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            await sut.ConfirmUser(unconfirmed.Id, new(["Test_x"]));
-            //Assert
-            Assert.That(unconfirmed, Is.Not.Null);
-            Assert.Multiple(() =>
-            {
-                Assert.That(unconfirmed.ConfirmedByAdmin);
-                Assert.That(unconfirmed.Rights.Single(), Is.EqualTo(new Right("Test", Operation.Execute)));
-            });
-        }
-
-        [Test]
-        public void ConfirmUser_UserDoesNotExist_ShouldActivateUser()
-        {
-            //Arrange
-            var userManager = new UserManager(null!, null!, null!);
-            var usersApi = new UsersApi(userManager);
-            var fakeAspUserManager = new FakeAspUserManager([_admin]);
-            var sut = new UsersController(usersApi, fakeAspUserManager,
-                new UserToUserResponseMapper(), new UserResponseToHalMapper(_admin));
-            //Act
-            async Task<Microsoft.AspNetCore.Mvc.ActionResult> update()
-                => await sut.ConfirmUser(Guid.Parse("191f1ad0-5360-4f86-8fcd-8099981a4ce2"), new(["Test_x"]));
-            //Assert
-            Assert.ThrowsAsync<KeyNotFoundException>(() => update());
-        }
     }
 }
--- AsyJob.Web/Auth/UsersController.cs
+++ AsyJob.Web/Auth/UsersController.cs
@@ -4,7 +4,6 @@ using AsyJob.Lib.Client.Abstract.Users;
 using AsyJob.Web.Auth.Rights;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Driver;
 
 namespace AsyJob.Web.Auth
 {
@@ -16,32 +15,29 @@ namespace AsyJob.Web.Auth
     [Route("api/users")]
     [ApiController]
     [Produces("application/hal+json")]
-    public class UsersController(
-        IUsersApi usersApi,
-        IAspUserManager userManager,
-        IMapper<User, UserResponse> userToUserResponseMapper,
-        IMapper<UserResponse, HalUserResponse> userToHalMapper) : ControllerBase
+    public class UsersController(IUsersApi usersApi, IAspUserManager userManager) : ControllerBase
     {
         private readonly IUsersApi _usersApi = usersApi;
         private readonly IAspUserManager _userManager = userManager;
-        private readonly IMapper<User, UserResponse> _userToUserResponseMapper = userToUserResponseMapper;
-        private readonly IMapper<UserResponse, HalUserResponse> _userToHalMapper = userToHalMapper;
 
         [HttpGet]
         [HasRights("Users_r")]
         public async Task<IEnumerable<HalUserResponse>> GetUsers()
         {
             var users = await _usersApi.GetAll();
-            return users.Select(_userToHalMapper.Map);
+            return users.Select(Map);
         }
+
+        private HalUserResponse Map(UserResponse response)
+            => new(response.Id, response.Username, response.Rights);
 
 
         [HttpPatch("{userId}")]
         [HasRights("Users_rw")]
-        public async Task<HalUserResponse> Update(Guid userId, UserUpdateRequest request)
+        public async Task<HalUserResponse> Update(Guid userId, UserUpdateRequest request) 
         {
             var result = await _usersApi.Update(new(userId, request.Username, request.Rights));
-            return _userToHalMapper.Map(result);
+            return Map(result);
         }
 
         [HttpGet("/unconfirmed")]
@@ -49,18 +45,18 @@ namespace AsyJob.Web.Auth
         public Task<IEnumerable<HalUserResponse>> GetUnconfirmedUsers()
         {
             var users = _userManager.Users.Where(u => !u.ConfirmedByAdmin).ToList();
-            var response = users.Select(_userToUserResponseMapper.Map)
-                .Select(_userToHalMapper.Map);
+            var response = users.Select(u => new HalUserResponse(u.Id, u.UserName ?? u.Email ?? u.Id.ToString(), u.Rights.Select(r => r.ToString())));
             return Task.FromResult(response.AsEnumerable());
         }
 
         [HttpPost("/unconfirmed/{userId}/confirm")]
         [HasRights("Users_w")]
-        public async Task<ActionResult> ConfirmUser(Guid userId, ConfirmUserRequest request)
+        public async Task<ActionResult> ConfirmUser(Guid userId, ConfirmUserRequest request) 
         {
-            var user = await _userManager.FindByIdAsync(userId)
-                ?? throw new KeyNotFoundException();
-            user.ConfirmedByAdmin = true;
+            var user = await _userManager.FindByIdAsync(userId);
+            if(user is null)
+                return NotFound(); 
+            user.ConfirmedByAdmin = true; 
             user.Rights = request.Rights.Select(r => new Right(r));
             await _userManager.UpdateAsync(user);
             return NoContent();
