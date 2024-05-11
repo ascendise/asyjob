13ad492f46dd8833cded0349f7f972600fb0b5bd
Ascendise <rapperido@gmail.com>
2024-05-11 12:22:14 +0200

    interface for reusable SetUp/TearDown task

---
+++ AsyJob.IntegrationTests/ISetUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyJob.IntegrationTests
+{
+    /// <summary>
+    /// Responsible for running a SetUp task before an integration test is run
+    /// </summary>
+    internal interface ISetUp
+    {
+        Task SetUp();
+    }
+}
---
+++ AsyJob.IntegrationTests/ITearDown.cs
@@ -0,0 +1,10 @@
+﻿namespace AsyJob.IntegrationTests
+{
+    /// <summary>
+    /// Responsible for running a TearDown task before an integration test is run
+    /// </summary>
+    internal interface ITearDown
+    {
+        Task TearDown();
+    }
+}
